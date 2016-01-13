741c842b814b6870c8cd6a215c9b6ce60c2d07bf
Cristian Duica <crisd0000@gmail.com>
2016-01-13 00:52:30 -0500

    Added animations

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* register the modules the application depends upon here*/
 angular.module('listings', []);
 
-/* register the application and inject all the necessary dependencies */
+/* register the application and inject all the necessary dependencies, including ngAnimate*/
 var app = angular.module('directoryApp', ['listings', 'ngAnimate']);
